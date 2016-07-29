985720c54d946238fbe25c5f1b998f9b15cb9445
A1oe <alyssalo88@gmail.com>
2016-07-29 14:59:23 -0700

    Update BlackJackPlayer.java

--- BlackJackPlayer.java
+++ BlackJackPlayer.java
@@ -1,3 +1,5 @@
+package black_jack;
+
 import java.util.ArrayList;
 
 /**
@@ -9,71 +11,70 @@ import java.util.ArrayList;
  *
  */
 public class BlackJackPlayer {
-	// fields
-	public int token;
-	public ArrayList <String> cards = new ArrayList <String>();
-	public String playerName;
-	public int tokenBet;
+    // fields
+    public int token;
+    public ArrayList <String> cards = new ArrayList <String>();
+    public String playerName;
+    public int tokenBet;
 
-	/*
-	 *  BlackjackPlayer - Constructor Method That Prints Out Player's Statuses
-	 *  
-	 *  @param - name : Name Of Player
-	 *          cardsIn : Cards That Player Has
-	 *  
-	 *  @return - none 
-	 */
-	public BlackJackPlayer(String name, int token) {
+    /*
+     *  BlackjackPlayer - Constructor Method That Prints Out Player's Statuses
+     *  
+     *  @param - name : Name Of Player
+     *          cardsIn : Cards That Player Has
+     *  
+     *  @return - none 
+     */
+    public BlackJackPlayer(String name, int token) {
 
-		// field playerName = name 
-		playerName = name;
+        // field playerName = name 
+        playerName = name;
 
-		// field token = token
-		this.token = token;
-	}
+        // field token = token
+        this.token = token;
+    }
 
-	/*
-	 *  BlackjackPlayer - Constructor Method That Prints Out Dealer's Statuses
-	 *  
-	 *  @param - name : Dealer
-	 *          cardsIn : Cards That Dealer Has
-	 *  
-	 *  @return - none 
-	 */
-	public BlackJackPlayer(String name) {
+    /*
+     *  BlackjackPlayer - Constructor Method That Prints Out Dealer's Statuses
+     *  
+     *  @param - name : Dealer
+     *          cardsIn : Cards That Dealer Has
+     *  
+     *  @return - none 
+     */
+    public BlackJackPlayer(String name) {
 
-		// field playerName = name 
-		playerName = name;
+        // field playerName = name 
+        playerName = name;
 
-	}
+    }
 
-	/*
-	 *	BlackjackPlayer - Constructor Method for Split Player
-	 *  
-	 *  @param - none
-	 *  
-	 *  @return - none  
-	 */
-	public BlackJackPlayer(){
-		token = 0;
-	}
+    /*
+     *    BlackjackPlayer - Constructor Method for Split Player
+     *  
+     *  @param - none
+     *  
+     *  @return - none  
+     */
+    public BlackJackPlayer(){
+        token = 0;
+    }
 
-	/*
-	 *  toString - Method That Tells How To Print BlackJackPlayer Objects
-	 * 
-	 *  @param - none
-	 *  
-	 *  @return - format : format Of How Object Is Printed
-	 */
-	public String toString() {
-		// TODO Auto-generated method stub
-		String format = playerName + " (" + token + ")" + " : ";
+    /*
+     *  toString - Method That Tells How To Print BlackJackPlayer Objects
+     * 
+     *  @param - none
+     *  
+     *  @return - format : format Of How Object Is Printed
+     */
+    public String toString() {
+        String format = playerName + " (" + token + ")" + " : ";
 
-		for(int i = 0; i < cards.size(); i++){
-			format = format + cards.get(i) + "   ";
-		}
+        for(int i = 0; i < cards.size(); i++){
+            format = format + cards.get(i) + "   ";
+        }
 
-		return format;
-	}
+        return format;
+    }
 
 }
